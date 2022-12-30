13f974a028697f48684461bc0ecf8e53074741a5
tj <tharunkumar200230@gmail.com>
2022-12-30 19:31:57 +0530

    todays task completed till single page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Login = () => {
+  return (
+    <div className='auth'>
+      <h1>Login</h1>
+      <form>
+        <input required type="text" placeholder='username'/>
+        <input required type="password" placeholder='password'/>
+        <button>login</button>
+        <p>this is an error!</p>
+        <span>Don't have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  )
+}
+
+export default Login
---
+++ client/src/pages/Write.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Write = () => {
+  return (
+    <div>Write</div>
+  )
+}
+
+export default Write
